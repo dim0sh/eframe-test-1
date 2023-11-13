edfd25bf4ca8170984a8b5dc8c7135067fc58ad7
Iain Dorsch <i_dorsch21@informatik.uni-kl.de>
2023-11-13 01:45:53 +0100

    added sub window

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,25 @@ use eframe::egui::{
 	Context,
 	CentralPanel,
 	Slider, SidePanel, TopBottomPanel,
-    Layout, Align
+    Layout, Align, Window, 
 };
-use eframe::{
-	Frame,
-	App
-};
+use eframe::{Frame,App};
 #[derive(PartialEq)]
 enum PanelSwitch {
     Age,
     Test,
 }
-
+#[derive(PartialEq)]
+enum SubPage {
+    None,
+    SubWindow,
+}
 
 struct MyApp {
     name: String,
     age: u32,
     panel_enum: PanelSwitch,
+    sub_enum: SubPage
 }
 impl Default for MyApp {
     fn default() -> Self {
@@ -26,6 +28,7 @@ impl Default for MyApp {
             name: "Arthur".to_owned(),
             age: 42,
             panel_enum: PanelSwitch::Age,
+            sub_enum: SubPage::None,
         }
     }
 }
@@ -45,9 +48,23 @@ impl App for MyApp {
                 };
             });
         });
-        SidePanel::right("myRightPanel").show(ctx,|ui| {
-            ui.label("Right Panel");
-        });
+        if self.panel_enum == PanelSwitch::Test {
+            SidePanel::right("myRightPanel").show(ctx,|ui| {
+                ui.label("Right Panel");
+                if ui.selectable_label(SubPage::SubWindow == self.sub_enum, "sub window").clicked() {
+                    match self.sub_enum {
+                        SubPage::None => self.sub_enum = SubPage::SubWindow,
+                        SubPage::SubWindow => self.sub_enum = SubPage::None
+                    }
+                    
+                }
+                if self.sub_enum == SubPage::SubWindow {
+                    Window::new("sub Window").show(ctx, |ui| {
+                        ui.label("hello world")
+                    });
+                }
+            });
+        }
         CentralPanel::default().show(ctx, |ui| {
             match self.panel_enum {
                 PanelSwitch::Age => {ui.heading("My egui Application");
@@ -80,5 +97,4 @@ impl App for MyApp {
 
 fn main() {
     eframe::run_native("myapp", eframe::NativeOptions::default(), Box::new(|cc| Box::new(MyApp::default())));
-    println!("Hello, world!");
 }
